116806f966e8bddb1bbdc80bf6f52159a3eb68ec
woo1052 <heeseungwoo@gmail.com>
2018-06-27 17:35:06 -0400

    error

--- src/containers/book-list.js
+++ src/containers/book-list.js
@@ -38,4 +38,4 @@ function mapDispatchToProps(dispatch) {
 }
 // Promote Booklist from a Component to a container - it needs to know
 //about this new dispatch method, selectBook, Make it available as a prop.
-export default connect(mapStateToProps)(BookList);
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'Javascript: The Good Parts'},
-    { title: 'Harry Potter'},
-    { title: 'The Dark Tower'},
-    { title: 'Eloquent Ruby'}
-  ]
+    { title: 'Javascript: The Good Parts', pages: 101 },
+    { title: 'Harry Potter', page: 39 },
+    { title: 'The Dark Tower', page: 85 },
+    { title: 'Eloquent Ruby', page 1 }
+  ];
 }
